898ddb713a597a7e51b149b41f954397426b84a5
jeremiah <jeremiahrichter@gmail.com>
2016-10-09 17:38:14 -0500

    add book.txt, basic draft of readFile()

--- src/FileReverser.java
+++ src/FileReverser.java
@@ -1,5 +1,28 @@
-/**
- * Created by jeremiah on 10/9/16.
- */
+import java.io.*;
+
 public class FileReverser {
+
+    private void readFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("book.txt"));
+            PrintWriter writer = new PrintWriter(new FileWriter("output.txt"));
+
+            String read, toWrite;
+
+            while ((read = reader.readLine()) != null) {
+                toWrite = reverseWord(read);
+                writer.write(toWrite);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (IOException e) {
+            System.out.println("Not able to write output.");
+        }
+    }
+
+    private String reverseWord(String input) {
+        return input;
+    }
+
+    public static void main(String args[]) {}
 }
