81d474fb997e2b95f4b23c5d47132b7fb09bced3
jeremiah <jeremiahrichter@gmail.com>
2016-10-09 21:00:47 -0500

    get paths right

--- src/FileReverser.java
+++ src/FileReverser.java
@@ -4,14 +4,15 @@ public class FileReverser {
 
     private void readFile() {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("book.txt"));
-            PrintWriter writer = new PrintWriter(new FileWriter("output.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("src/book.txt"));
+            PrintWriter writer = new PrintWriter(new FileWriter("out/production/FileIO/output.txt"));
 
             String read, toWrite;
 
             while ((read = reader.readLine()) != null) {
                 toWrite = reverseWord(read);
                 writer.write(toWrite);
+                writer.write("\n");
             }
 
             reader.close();
@@ -27,5 +28,9 @@ public class FileReverser {
         return (new StringBuffer(input)).reverse().toString();
     }
 
-    public static void main(String args[]) {}
+    public static void main(String args[]) {
+        FileReverser f = new FileReverser();
+        f.readFile();
+        System.out.println("File is reversed, thank you!");
+    }
 }
